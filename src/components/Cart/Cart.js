4d8d30c5d7faf8e982c86a3cd87271760ad8a05e
hiddenplayer <ngocthien.dnt@gmail.com>
2022-02-25 22:43:22 +0700

    show cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,104 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import "./Cart.css";
+import rm from "../../assets/remove.png";
+
 function Cart() {
-  const [cart, setCart] = useState([]);
-  const [current, setCurrent] = useState("");
+  const [cart, setCart] = useState(
+    JSON.parse(window.localStorage.getItem("cart")) ?? []
+  );
+
   const { qr } = useParams();
-  // if (qr != current) {
-  //   setCart([...qr]);
-  //   setCurrent(qr);
-  // }
-  return <div>Products: {qr}</div>;
+
+  useEffect(() => {
+    if ([].includes(qr)) return;
+    fetch("http://localhost:8081/products/" + qr)
+      .then((res) => res.json())
+      .then((result) => {
+        updateCart(result[0].name, result[0].price, 1);
+      });
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function checkContain() {
+    cart.map((p) => {
+      console.log(p[0] + "/" + qr + "/");
+      if (p[0] === qr) {
+        console.log("true");
+        return "true";
+      }
+    });
+    console.log("false");
+    return "false";
+  }
+
+  function updateCart(name, price, quantity) {
+    let existing = cart.slice();
+    let update = [...existing, [name, price, quantity]];
+    return setCart(update);
+  }
+
+  function remove(index) {
+    let existing = cart.slice();
+    existing.splice(index, 1);
+    return setCart(existing);
+  }
+
+  function numberWithCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(",");
+  }
+
+  function sum() {
+    let total = 0;
+    cart.map((p) => {
+      total += p[1];
+    });
+    return total;
+  }
+  return (
+    <div className="cart">
+      <h1> GIỎ HÀNG</h1>
+
+      <div className="cart-table">
+        <table cellspacing="0" cellpadding="0" align="center">
+          <thead>
+            <th>-</th>
+            <th>SẢN PHẨM</th>
+            <th>GIÁ</th>
+            <th>SỐ LƯỢNG</th>
+            <th>TẠM TÍNH</th>
+          </thead>
+          <tbody>
+            {cart.map((p, index) => {
+              return (
+                <tr>
+                  <td>
+                    <img src={rm} onClick={() => remove(index)} />
+                  </td>
+                  <td>{p[0]}</td>
+                  <td>{numberWithCommas(p[1])} đ</td>
+                  <td>{p[2]}</td>
+                  <td>{numberWithCommas(p[2] * p[1])} đ</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <div className="cart-total">
+          <p>Tổng cộng: {numberWithCommas(sum())} đ</p>
+          <p>{checkContain()}</p>
+          <button>THANH TOÁN</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -8,10 +8,9 @@ import "./Product.css";
 export const Product = ({ product }) => {
   function numberWithCommas(x) {
     var parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(",");
   }
-
   return (
     <div className="product">
       <img className="product-img" src={model} />

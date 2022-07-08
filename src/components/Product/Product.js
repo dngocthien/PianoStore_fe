a549f7433f0da93455e60ed99692ae53d6c741a3
hiddenplayer <ngocthien.dnt@gmail.com>
2022-07-08 22:28:43 +0700

    update remain status

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import model from "../../assets/model.jpg";
 import cart from "../../assets/cart.png";
+import { useNavigate } from "react-router-dom";
 
 import "./Product.css";
 
 export const Product = ({ product }) => {
+  const navigate = useNavigate();
+
+  function toCart() {
+    if (product.remain < 1) {
+      alert("Sản phẩm đang tạm thời hết hàng!");
+    } else {
+      navigate(`/cart/${product.name}`, { state: { qr: product.name } });
+    }
+  }
+
   function numberWithCommas(x) {
     var parts = x.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -20,9 +31,9 @@ export const Product = ({ product }) => {
       <p>{product.name}</p>
       <p className="product-price">{numberWithCommas(product.price)}đ</p>
       <p>{product.remain ? "Còn hàng" : "Hết hàng"}</p>
-      <Link to={`/cart/${product.name}`} state={{ qr: product.name }}>
-        <img className="product-add" src={cart} />
-      </Link>
+      <img className="product-add" src={cart} onClick={() => toCart()} />
+      {/* <Link to={`/cart/${product.name}`} state={{ qr: product.name }}>
+      </Link> */}
       <p className="product-secrete">.</p>
     </div>
   );
